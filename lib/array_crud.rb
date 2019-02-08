02c8ef46573225802b2499eda7bed313659fa75f
Robert Pedersen <rob@worldcrimeleague.org>
2019-02-08 18:03:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  ["wow","I","am","really","learning","arrays!"]
+  array.pop("arrays!")
   
 end
 
